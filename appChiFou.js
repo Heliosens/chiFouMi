3c75865ad2970217e48bf293418797fcb168f590
Sylvie-B <unsolo@hotmail.fr>
2021-12-01 15:06:14 +0100

    comparaison dans switch

--- appChiFou.js
+++ appChiFou.js
@@ -28,16 +28,41 @@ for( let hand of hands){
         result.style.visibility = "hidden";
         // user choice
         frames[0].style.backgroundImage = hand.style.backgroundImage;
-        console.log(hand.style.backgroundImage)// user choice url;
         // computer random
         frames[1].style.backgroundImage = img[Math.floor(Math.random() * hands.length)]; // random choice url;
         // check winner
-        switch (hand.style.backgroundImage){
-            case img[0] :
-                if(frames[1].style.backgroundImage === img[1]){     // computer => scissor
-                    result.style.visibility = "visible";
+        // console.log(hand.style.backgroundImage);
+        switch (hand.style.backgroundImage){        // switch of player choice
+            case hands[0].style.backgroundImage :      // case paper
+                console.log('paper');
+                if(frames[1].style.backgroundImage === hands[1].style.backgroundImage){
+                    console.log("comp = scissor");
                 }
+                else if (frames[1].style.backgroundImage === hands[2].style.backgroundImage){
+                    console.log("comp = stone");
+                }
+                break;
+            case hands[1].style.backgroundImage :       // case scissor
+                console.log('scissor');
+
+                break;
+            case hands[2].style.backgroundImage :       // case paper
+                console.log('stone');
+                break;
         }
     })
 }
 
+// case frames[1].style.backgroundImage :
+// if(frames[1].style.backgroundImage === ){     // computer => scissor
+//     result.style.visibility = "visible";
+//     result.innerHTML = "Computer win";
+// }
+// else if (frames[1].style.backgroundImage === "url('stone.png')"){   // computer => stone
+//     result.style.visibility = "visible";
+//     result.innerHTML = "You win";
+// }
+// else {
+//     result.style.visibility = "visible";
+//     result.innerHTML = "Match nul";
+// }
