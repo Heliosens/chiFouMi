01de1bffce611df95561d5879d1aff542642c67c
Sylvie-B <unsolo@hotmail.fr>
2021-12-02 13:39:58 +0100

    use ternaire in switch case

--- appChiFou.js
+++ appChiFou.js
@@ -29,40 +29,27 @@ for( let i = 0 ; i < hands.length ; i++){
         // user choice
         frames[0].style.backgroundImage = hands[i].style.backgroundImage;
         // computer random
-        let x = Math.floor(Math.random() * hands.length);
-        frames[1].style.backgroundImage = img[x]; // random choice url;
+        let x = Math.floor(Math.random() * hands.length);   // x = comp. choice
+        frames[1].style.backgroundImage = img[x];           // url[x];
 
         // check winner
         if (i === x){
             result.innerHTML = "Match nul";
         }
         else {
+            let w;
             switch (i){        // switch of player choice
                 case 0 :                                       // case paper
-                    if(x === 1){     // vs scissor
-                        addPoint(1);
-                    }
-                    else if (x === 2){   // vs stone
-                        addPoint(0);
-                    }
+                    w = x === 1 ? 1 : 0;
                     break;
                 case 1 :                                           // case scissor
-                    if(x === 0){     // vs paper
-                        addPoint(0);
-                    }
-                    else if (x === 2){   // vs stone
-                        addPoint(1);
-                    }
+                    w = x === 0 ? 0 : 1;
                     break;
                 case 2 :                                           // case stone
-                    if(x === 0){     // vs paper
-                        addPoint(1);
-                    }
-                    else if (x === 1){   // vs scissor
-                        addPoint(0);
-                    }
+                    w = x === 0 ? 1 : 0;
                     break;
             }
+            addPoint(w);
         }
     })
 }
@@ -79,8 +66,6 @@ function addPoint (w){
 
 // create an element for user or comp
 function addResult (){
-
     // big div in historic
     historic.appendChild(historic);
 }
-
