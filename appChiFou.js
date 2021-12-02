dcb4ee64e77eb385531474e96aa9e11a17ceca26
Sylvie-B <unsolo@hotmail.fr>
2021-12-02 12:32:52 +0100

    create add point function

--- appChiFou.js
+++ appChiFou.js
@@ -12,16 +12,16 @@ let img = [
 ];
 
 // to display result
-let result = document.getElementById('result');
-let points = document.getElementsByClassName('point');
-let historic = document.getElementById('historic');
+let result = document.getElementById('result');     // red frame
+let points = document.getElementsByClassName('point');    // number of points
+let historic = document.getElementById('historic');    // choice user & comp + winner
 
 // display symbols
 for(let i = 0 ; i < hands.length ; i++){
     hands[i].style.backgroundImage = img[i];
 }
 
-// on click display user & computer choice
+// on click : display user & computer choice
 // count point -> display
 // display result
 for( let i = 0 ; i < hands.length ; i++){
@@ -38,34 +38,28 @@ for( let i = 0 ; i < hands.length ; i++){
         }
         else {
             switch (i){        // switch of player choice
-                case 0 :                                           // case paper
+                case 0 :                                       // case paper
                     if(x === 1){     // vs scissor
-                        result.innerHTML = "Computer win";
-                        points[1].innerHTML = (parseInt(points[1].innerHTML) + 1).toString();
+                        addPoint(1);
                     }
                     else if (x === 2){   // vs stone
-                        result.innerHTML = "You win !!!";
-                        points[0].innerHTML = (parseInt(points[0].innerHTML) + 1).toString();
+                        addPoint(0);
                     }
                     break;
                 case 1 :                                           // case scissor
                     if(x === 0){     // vs paper
-                        result.innerHTML = "You win !!!";
-                        points[0].innerHTML = (parseInt(points[0].innerHTML) + 1).toString();
+                        addPoint(0);
                     }
                     else if (x === 2){   // vs stone
-                        result.innerHTML = "Computer win";
-                        points[1].innerHTML = (parseInt(points[1].innerHTML) + 1).toString();
+                        addPoint(1);
                     }
                     break;
                 case 2 :                                           // case stone
                     if(x === 0){     // vs paper
-                        result.innerHTML = "Computer win";
-                        points[1].innerHTML = (parseInt(points[1].innerHTML) + 1).toString();
+                        addPoint(1);
                     }
                     else if (x === 1){   // vs scissor
-                        result.innerHTML = "You win !!!";
-                        points[0].innerHTML = (parseInt(points[0].innerHTML) + 1).toString();
+                        addPoint(0);
                     }
                     break;
             }
@@ -73,11 +67,20 @@ for( let i = 0 ; i < hands.length ; i++){
     })
 }
 
-function addResult (x,i,w){
-    let items = [x, i];
-    for(let item of items){
-        let div = document.createElement("div");
-        div.style.backgroundImage = hands[item].style.backgroundImage;
-        historic.appendChild(div);
-    }
+/**
+ * display who is the winner and
+ * add 1 pt to winner w = 0 for player & w = 1 for computer
+ * @param w
+ */
+function addPoint (w){
+    result.innerHTML = w === 0 ? "You win !!!" :  "Computer win";
+    points[w].innerHTML = (parseInt(points[w].innerHTML) + 1).toString();
 }
+
+// create an element for user or comp
+function addResult (){
+
+    // big div in historic
+    historic.appendChild(historic);
+}
+
